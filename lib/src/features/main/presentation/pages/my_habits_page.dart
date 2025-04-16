6c7ad673ee34c0f61c24cb55337ec7f1be8664d1
AjDDcatoria <ardedicatoria9@nemsu.edu.ph>
2025-04-16 15:53:14 +0800

    list in my habit

--- lib/src/features/main/presentation/pages/my_habits_page.dart
+++ lib/src/features/main/presentation/pages/my_habits_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habitly/src/common/habit_button.dart';
+import 'package:habitly/src/constants/sizes.dart';
+import 'package:habitly/src/features/main/bloc/habit/habit_bloc.dart';
 import 'package:habitly/src/features/main/presentation/widgets/main_screen_appbar_widget.dart';
 
 class MyHabitPage extends StatefulWidget {
@@ -16,11 +20,29 @@ class _MyHabitPageState extends State<MyHabitPage> {
         title: 'My Habits',
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
       ),
-      body: Center(
-        child: Text(
-          'My Habit Page',
-          style: Theme.of(context).textTheme.headlineLarge,
-        ),
+      body: BlocBuilder<HabitBloc, HabitState>(
+        builder: (context, state) {
+          return Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: AppSizes.paddingLg),
+              itemCount: state.habits.length,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              separatorBuilder:
+                  (context, index) =>
+                      SizedBox(height: AppSizes.defaultBtwItems),
+              itemBuilder: (context, index) {
+                final habit = state.habits[index];
+                return HabitButton(
+                  icon: habit.icon,
+                  title: habit.title,
+                  color: habit.bgColor,
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
